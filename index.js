db902568b336e4c0d0409cd684fc61d4e2295929
nieskens-hva <111011168+nieskens-hva@users.noreply.github.com>
2022-12-13 13:45:41 +0100

    converted to hsl-colors

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ darkModeButton.addEventListener('click', setDarkMode);
 function setDarkMode() {
     // root.style.setProperty('--primary', 'rgb(200, 200, 200)');
     root.style.setProperty('color-scheme', 'dark');
-    root.style.setProperty('--primary', 'rgb(200, 200, 200)');
-    root.style.setProperty('--secondary', 'rgb(23, 23, 23)');
-    root.style.setProperty('--body-bg', 'rgb(25, 25, 25)');
-    root.style.setProperty('--header-bg', 'rgb(0, 0, 0)');
+    root.style.setProperty('--primary', 'hsl(0, 0%, 78%)');
+    root.style.setProperty('--secondary', 'hsl(0, 0%, 9%)');
+    root.style.setProperty('--body-bg', 'hsl(0, 0%, 10%)');
+    root.style.setProperty('--header-bg', 'hsl(0, 0%, 0%)');
     root.style.setProperty('--section-bg', 'rgb(0, 0, 0)');
-    root.style.setProperty('--shadow', '0px 3px 5px 0px rgb(14, 14, 14)');
+    root.style.setProperty('--shadow', '0px 3px 5px 0px hsl(0, 0%, 5%)');
     
 }
 
@@ -26,12 +26,12 @@ lightModeButton.addEventListener('click', setLightMode);
 function setLightMode() {
     // root.style.setProperty('--primary', 'rgb(200, 200, 200)');
     root.style.setProperty('color-scheme', 'light');
-    root.style.setProperty('--primary', 'rgb(0, 36, 79)');
-    root.style.setProperty('--secondary', 'rgb(255, 255, 255)');
-    root.style.setProperty('--body-bg', 'rgb(239, 240, 244)');
-    root.style.setProperty('--header-bg', 'rgb(220, 224, 232)');
-    root.style.setProperty('--section-bg', 'rgb(220, 224, 232)');
-    root.style.setProperty('--shadow', '0px 3px 5px 0px rgb(170, 170, 170)');
+    root.style.setProperty('--primary', 'hsl(213, 100%, 15%)');
+    root.style.setProperty('--secondary', 'hsl(0, 0%, 88%)');
+    root.style.setProperty('--body-bg', 'hsl(228, 19%, 95%)');
+    root.style.setProperty('--header-bg', 'hsl(220, 21%, 89%)');
+    root.style.setProperty('--section-bg', 'hsl(220, 21%, 89%)');
+    root.style.setProperty('--shadow', '0px 3px 5px 0px hsl(0, 0%, 67%)');
 }
 
 
