db2a56636f0cf32238e1934b0192b01b8742bcee
nieskens-hva <111011168+nieskens-hva@users.noreply.github.com>
2022-12-13 12:43:08 +0100

    styling fixes

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const root = document.documentElement;
 const darkModeButton = document.querySelector('#dark-mode');
 const lightModeButton = document.querySelector('#light-mode');
 
-const colorScheme = root.style.getPropertyValue('color-scheme');
-console.log("Color scheme: ", colorScheme);
+// const colorScheme = root.style.getPropertyValue('color-scheme');
+// console.log("Color scheme: ", colorScheme);
 
 darkModeButton.addEventListener('click', setDarkMode);
 
@@ -14,6 +14,7 @@ function setDarkMode() {
     root.style.setProperty('--primary', 'rgb(200, 200, 200)');
     root.style.setProperty('--secondary', 'rgb(23, 23, 23)');
     root.style.setProperty('--body-bg', 'rgb(25, 25, 25)');
+    root.style.setProperty('--header-bg', 'rgb(0, 0, 0)');
     root.style.setProperty('--section-bg', 'rgb(0, 0, 0)');
     root.style.setProperty('--shadow', '0px 3px 5px 0px rgb(14, 14, 14)');
     
@@ -28,6 +29,7 @@ function setLightMode() {
     root.style.setProperty('--primary', 'rgb(0, 36, 79)');
     root.style.setProperty('--secondary', 'rgb(255, 255, 255)');
     root.style.setProperty('--body-bg', 'rgb(239, 240, 244)');
+    root.style.setProperty('--header-bg', 'rgb(220, 224, 232)');
     root.style.setProperty('--section-bg', 'rgb(220, 224, 232)');
     root.style.setProperty('--shadow', '0px 3px 5px 0px rgb(170, 170, 170)');
 }
